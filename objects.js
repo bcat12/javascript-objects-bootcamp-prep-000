8833b7c9d88774016355410e510bbd6950b7532f
Brian Caterfino <bcaterfino@gmail.com>
2018-03-08 22:06:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle){
- deleteplaylist[artistName]
+ deleteplaylist[artistName];
  return playlist
 }
